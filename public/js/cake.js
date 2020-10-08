b99e88087be8ea6d8b8354cd9e3026d17aae603d
Jeregrande <jeregrande@yahoo.com>
2020-10-08 12:11:54 -0700

    Special K

--- public/js/cake.js
+++ public/js/cake.js
@@ -164,7 +164,7 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('Sep 19, 2020 01:00:00').getTime(),
+let countDown = new Date('Oct 7, 2020 01:00:00').getTime(),
     x = setInterval(function() {    
 
       let now = new Date().getTime(),
